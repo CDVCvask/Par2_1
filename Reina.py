dc149f512d290f6ad8b3e97f1b0dc04d73af80d2
CDVC <cesarcamposeco415@gmail.com>
2025-09-08 10:13:59 -0600

    Candidata

--- Reina.py
+++ Reina.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 class Reina:
+    def __init__(self,codigo,nombre,edad,insitucion,municipio):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.edad = edad
+        self.insitucion = insitucion
+        self.municipio = municipio
+    #código, nombre, edad, institución educativa, municipio
     pass
 class Jurado:
     pass
@@ -9,6 +16,8 @@ class Concurso_Reinas_app:
         self.ventana.title("Reina")
         self.ventana.geometry("400x400")
         self.menu()
+        self.titulo = tk.Label(self.ventana, text="Reina")
+        self.titulo.place(x= 100, y=100)
         self.ventana.mainloop()
     def menu(self):
         barra = tk.Menu(self.ventana)
