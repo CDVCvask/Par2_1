bb70a5a672abeb2ccbacd2c77574b66568c00159
CDVC <cesarcamposeco415@gmail.com>
2025-09-08 10:30:47 -0600

    Calificando 2

--- Reina.py
+++ Reina.py
@@ -8,13 +8,37 @@ class Reina:
         self.municipio = municipio
     #código, nombre, edad, institución educativa, municipio
 class Jurado:
-    pass
+    def __init__(self,codigo,nombre,especialidad):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.especialidad = especialidad
 class Mostrar_Reinas:
     def __init__(self):
         self.Reinas = []
         self.Jurados = []
     def Puntaje_Reinas(self,reina,jurado,cultura,proyeccion,entrevista):
-        pass
+        allow = 0
+        allow2 = 0
+        try:
+            cultura = int(cultura)
+            entrevista = int(entrevista)
+            proyeccion = int(proyeccion)
+        except ValueError:
+            print("Tipo de dato ingresado no es valido")
+        for Reina in self.Reinas:
+            if Reina.codigo == reina:
+                allow = 1
+        for Jurado in self.Jurados:
+            if Jurado.codigo == jurado:
+                allow2 = 0
+        if allow == 1 and allow2 == 1:
+            if cultura >= 0 and cultura <= 10:
+                if entrevista >= 0 and entrevista <= 10:
+                    if proyeccion >= 0 and proyeccion <= 10:
+                        promedio =  cultura + entrevista + proyeccion
+                        promedio = promedio / 3
+        else:
+            print("El codigo de la reina o el jurado no es valido")
     def Agregar_Reina(self,reina):
         self.Reinas.append(reina)
     def Agregar_Jurado(self,jurado):
