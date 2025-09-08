ee3259fb936b01a05a7ff63c534a3b24eb4d3d37
CDVC <cesarcamposeco415@gmail.com>
2025-09-08 10:10:34 -0600

    menu

--- Reina.py
+++ Reina.py
@@ -4,4 +4,12 @@ class Reina:
 class Jurado:
     pass
 class Concurso_Reinas_app:
-    pass
+    def __init__(self):
+        self.ventana = tk.Tk()
+        self.ventana.title("Reina")
+        self.ventana.geometry("400x400")
+        self.menu()
+        self.ventana.mainloop()
+    def menu(self):
+        barra = tk.Menu(self.ventana)
+
